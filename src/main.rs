076951efa8062b1ec410fcaad27c33cbb9035f82
Kevin Müller <mkevin2006@gmail.com>
2024-05-31 17:17:18 +0200

    Add debug function for writing an image buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use std::{fs::File, io::BufReader};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+struct ImageBuffer {
+    width: usize,
+    height: usize,
+    buffer: Box<[u8]>,
+}
 
 fn main() {
     let f = File::open("vid/OpeningManim.mp4").unwrap();
@@ -10,4 +18,16 @@ fn main() {
     println!("Timescale: {}", mp4.moov.mvhd.timescale);
     println!("Size: {}", mp4.size());
     println!("Duration: {:?}", mp4.duration());
+}
+
+fn write_image_buffer(image_buffer: ImageBuffer) -> std::io::Result<()> {
+    let path = "debug.ppm";
+    let header = format!("P6\n{} {} 255\n", image_buffer.width, image_buffer.height);
+
+    let mut file = File::create(path)?;
+
+    file.write(header.as_bytes())?;
+    file.write(&image_buffer.buffer)?;
+
+    Ok(())
 }
