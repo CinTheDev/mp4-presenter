f566216ea6b74f19f46a0bc47c3ed0e70f7a9bd5
Kevin Müller <mkevin2006@gmail.com>
2024-05-31 16:02:24 +0200

    Copy mp4 simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs::File, io::BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("vid/OpeningManim.mp4").unwrap();
+    let size = f.metadata().unwrap().len();
+    let reader = BufReader::new(f);
+
+    let mp4 = mp4::Mp4Reader::read_header(reader, size).unwrap();
+
+    println!("Timescale: {}", mp4.moov.mvhd.timescale);
+    println!("Size: {}", mp4.size());
+    println!("Duration: {:?}", mp4.duration());
 }
