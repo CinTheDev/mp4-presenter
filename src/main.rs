e1d26c1d03f308b0320d85e72621defbe59578e1
Kevin Müller <mkevin2006@gmail.com>
2024-06-01 13:50:56 +0200

    Instance decoder in main

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,13 @@ use std::thread::sleep;
 fn main() {
     ffmpeg_next::init().unwrap();
 
+    let decoder = VideoDecoder::new("vid/OpeningManim.mp4").expect("Failed to init decoder");
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "MP4-Presenter",
         native_options,
-        Box::new(|cc| Box::new(EguiApp::new(cc))),
+        Box::new(|cc| Box::new(EguiApp::new(cc, decoder))),
     ).unwrap();
 
     /*
