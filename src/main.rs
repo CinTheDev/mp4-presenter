a3eb53ea1c6d3651ef866e102c8753fa4ba15917
Kevin Müller <mkevin2006@gmail.com>
2024-06-08 21:50:47 +0200

    Make app fullscreen

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ use ui::EguiApp;
 fn main() {
     ffmpeg_next::init().unwrap();
 
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder {
+            fullscreen: Some(true),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
     eframe::run_native(
         "MP4-Presenter",
         native_options,
