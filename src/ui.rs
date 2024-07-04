7f4ed4d6b5f8ca1e5349c3a3786c918a31204e0a
Kevin Müller <mkevin2006@gmail.com>
2024-07-04 19:27:37 +0200

    Copy get_all_files() from old code

---
+++ src/ui.rs
@@ -0,0 +1,22 @@
+fn get_all_files(dir: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
+    let dir_entries = std::fs::read_dir(dir).expect("Cannot open animation source directory");
+
+    for read_entry in dir_entries {
+        let entry = read_entry.expect("Cannot read animation source file");
+        let path = entry
+            .path()
+            .canonicalize()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_owned();
+        
+        result.push(path);
+    }
+
+    result.sort_unstable();
+
+    result
+}
